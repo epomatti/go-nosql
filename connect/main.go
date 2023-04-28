899d599fd29fdf84356db11bf1009376fd846f52
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-04-28 17:53:40 -0300

    connect disconnect

--- connect/main.go
+++ connect/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
 
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	clientOptions := options.Client().ApplyURI("mongodb://127.0.0.1:27017")
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		err := client.Disconnect(context.TODO())
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(">> Disconnected!")
+		}
+	}()
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(">> Connected!")
 }
